8ffdac369c9a574208eb7d1ad7cc2af78941421a
Slava <srozmanics@gmail.com>
2026-06-17 21:12:30 +0300

    smooth scroll

--- script.js
+++ script.js
@@ -9,23 +9,35 @@ cards.forEach(card => {
         const x = e.clientX - rect.left;
         const y = e.clientY - rect.top;
 
-        const rotateY =
-            ((x / rect.width) - 0.5) * 12;
-
-        const rotateX =
-            ((y / rect.height) - 0.5) * -12;
+        const rotateY = ((x / rect.width) - 0.5) * 12;
+        const rotateX = ((y / rect.height) - 0.5) * -12;
 
         card.style.transform =
-            `perspective(1000px)
-            rotateX(${rotateX}deg)
-            rotateY(${rotateY}deg)
-            translateY(-10px)`;
+        `perspective(1000px)
+        rotateX(${rotateX}deg)
+        rotateY(${rotateY}deg)
+        translateY(-10px)`;
     });
 
     card.addEventListener("mouseleave", () => {
 
         card.style.transform =
-            "perspective(1000px) rotateX(0) rotateY(0)";
+        `perspective(1000px)
+        rotateX(0deg)
+        rotateY(0deg)`;
+    });
+
+});
+
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+scrollTopBtn.addEventListener("click", e => {
+
+    e.preventDefault();
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
     });
 
 });
